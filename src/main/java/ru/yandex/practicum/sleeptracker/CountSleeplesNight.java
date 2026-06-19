9336f2b039a59377f4f0fa60ce49ec94fdb2f576
Yashar <yeromancov@gmail.com>
2026-06-19 15:55:26 +0300

    Исправить ошибки тестов

--- src/main/java/ru/yandex/practicum/sleeptracker/CountSleeplesNight.java
+++ src/main/java/ru/yandex/practicum/sleeptracker/CountSleeplesNight.java
@@ -12,9 +12,10 @@ public class CountSleeplesNight implements Function<List<SleepingSession>, Sleep
     @Override
     public SleepAnalysisResult apply(List<SleepingSession> sleepingSessions) {
         SleepingSession firstSession = sleepingSessions.get(0);
+        SleepingSession lastSession = sleepingSessions.get(sleepingSessions.size() - 1);
         LocalDate firstNight = firstSession.getStart().toLocalTime().isAfter(LocalTime.NOON)
                 ? firstSession.getStart().toLocalDate().plusDays(1) : firstSession.getStart().toLocalDate();
-        LocalDate lastNight = sleepingSessions.get(sleepingSessions.size() - 1).getStart().toLocalDate();
+        LocalDate lastNight = lastSession.getStart().toLocalTime().isAfter(LocalTime.NOON) ? lastSession.getStart().toLocalDate().plusDays(1) : lastSession.getStart().toLocalDate();
         long nightWithotSleeep = firstNight.datesUntil(lastNight.plusDays(1))
                 .filter(localDate -> sleepingSessions.stream()
                         .noneMatch(session -> SleepInstrument.isNightSleep(session, localDate)))
